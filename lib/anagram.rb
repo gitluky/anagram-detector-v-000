2b60172f984b16170791ee317023330ece1b88ec
Jonathan Luk <jluk87@gmail.com>
2018-03-14 19:41:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,3 @@
 class Anagram
-  attr_accessor :word
-
-  def initialize(word)
-    @word = word
-  end
-
-  def match(array)
-    array.select {|maybe_anagram| maybe_anagram.split(//).sort == @word.split(//).sort}
-  end
-
 
 end
